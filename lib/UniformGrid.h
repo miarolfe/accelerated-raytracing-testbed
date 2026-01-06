60ef1ce9e8a80e5de6b1a8ff91fa2d9a08a480bc
Mia Rolfe <hello@miarolfe.com>
2026-01-06 14:26:19 +0000

    Uniform grid implementation v1 + AABB Hit() fix

--- lib/UniformGrid.h
+++ lib/UniformGrid.h
@@ -3,28 +3,56 @@
 #include "Common.h"
 #include "IRayHittable.h"
 #include "RayHitResult.h"
+#include "Vec3.h"
+#include "Vec3Int.h"
+#include <set>
 
 namespace ART
 {
 
+struct UniformGridEntry
+{
+    std::set<std::shared_ptr<IRayHittable>> hittables;
+};
+
 class UniformGrid : public IRayHittable
 {
 public:
-    UniformGrid();
+    UniformGrid(const std::vector<std::shared_ptr<IRayHittable>>& objects);
 
-    UniformGrid(std::shared_ptr<IRayHittable> hittable);
-
-    UniformGrid(std::vector<std::shared_ptr<IRayHittable>>& objects);
-
-    void Clear();
-
-    void Add(std::shared_ptr<IRayHittable> hittable);
+    ~UniformGrid();
 
     bool Hit(const Ray& ray, Interval ray_t, RayHitResult& out_result) const override;
 
     std::vector<std::shared_ptr<IRayHittable>>& GetObjects();
 
     AABB BoundingBox() const override;
+
+    void Create();
+
+    void Destroy();
+
+protected:
+    bool CellHit(const UniformGridEntry& entry, const Ray& ray, Interval ray_t, RayHitResult& out_result) const;
+
+    Vec3 DetermineCellSize() const;
+
+    Vec3Int Calculate3DIndex(Vec3 position) const;
+
+    Vec3Int Calculate3DIndex(std::size_t one_dimensional_index) const;
+
+    std::size_t Calculate1DIndex(Vec3 position) const;
+
+    std::size_t Calculate1DIndex(Vec3Int three_dimensional_index) const;
+
+    AABB m_bounding_box;
+    std::vector<std::shared_ptr<IRayHittable>> m_objects;
+    UniformGridEntry* m_grid;
+    Vec3 m_cell_size;
+    std::size_t m_num_x_cells;
+    std::size_t m_num_y_cells;
+    std::size_t m_num_z_cells;
+    bool m_is_grid_valid = false;
 };
 
 } // namespace ART
--- src/ART.cpp
+++ src/ART.cpp
@@ -6,6 +6,7 @@
 #include "../lib/Material.h"
 #include "../lib/RayHittableList.h"
 #include "../lib/Sphere.h"
+#include "../lib/UniformGrid.h"
 #include "../lib/Vec3.h"
 
 void Scene1()
@@ -101,11 +102,46 @@ void Scene3()
     camera.Render(scene);
 }
 
+void Scene4()
+{
+    ART::CameraSetupParams camera_setup_params
+    {
+        600,                            // image_width
+        400,                            // image_height
+        ART::Colour(0.7, 0.8, 1.0),     // background_colour
+        20.0,                           // vertical_fov
+        100,                            // samples_per_pixel
+        25,                             // max_ray_bounces
+        ART::Point3(13.0, 2.0, 3.0),    // look_from
+        ART::Point3(0.0, 0.0, 0.0),     // look_at
+        ART::Vec3(0.0, 1.0, 0.0),       // up
+        0.0,                            // defocus_angle
+        10.0                            // focus_distance
+    };
+    ART::Camera camera(camera_setup_params);
+
+    ART::RayHittableList scene;
+
+    std::shared_ptr<ART::Texture> checker_texture = std::make_shared<ART::CheckerTexture>(
+        0.5, ART::Colour(0.0), ART::Colour(1.0));
+
+    scene.Add(std::make_shared<ART::Sphere>(ART::Point3(0.0, -10.0, 0.0), 10.0, std::make_shared<ART::LambertianMaterial>(checker_texture)));
+    scene.Add(std::make_shared<ART::Sphere>(ART::Point3(0.0, 1.0, 0.0), 1.0, std::make_shared<ART::DielectricMaterial>(1.5)));
+    scene.Add(std::make_shared<ART::Sphere>(ART::Point3(0.0, 1.0, 0.0), 0.8, std::make_shared<ART::DielectricMaterial>(1.0 / 1.5)));
+
+    ART::UniformGrid uniform_grid(scene.GetObjects());
+    uniform_grid.Create();
+
+    camera.Render(uniform_grid);
+
+    uniform_grid.Destroy();
+}
+
 int main()
 {
     ART::Logger::Get().LogInfo("Booting up");
 
-    Scene3();
+    Scene4();
 
     ART::Logger::Get().Flush();
 }
